7e752caf5c3ff3b204a1324b9e8abeb267bac27d
Anshul Tripathi <tripathianshul06@gmail.com>
2022-04-22 16:22:51 +0530

    Learning Reactjs Day 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ReactDOM.render('kya dikhana h','kaha dikhana hai','callback');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reactdom.render srf ek element krata hai render.enclose krdo ek tag m.hm array m bhi daal skte h tags ko , k sth
+// ReactDOM.render(
+// <div><h1>Hello world !!!</h1><p>i am learning reactjs</p><h2>i will definitely get 12 lpa</h2></div>,document.getElementById("root"));
+
+
+// ReactDOM.render( React.createElement("h1",   hm isliye react import krare kyunki babel esa kr rha
+// null,
+// 'Anshul Tripathi'),
+// document.getElementById("root"));
+
+
+//babel converts modern js into js understandable by browser.babel is acompiler . webpack combines all our files in one place.
+
+// var h1 = document.createElement("h1");    2 method for adding data
+
+// h1.innerHTML = "Anshul Tripathi";
+// document.getElementById("root").appendChild(h1);
+
+
+
+//jsx means js extension or js xml
+
+
+
+ // Fragments ek extra div bnane se bchata hai. vrna fr css m dikkt hogi.faster and consumes less memory  
+
+//<> </> yeh synatic sugar form of fragments hai
+// ReactDOM.render(   
+//     <React.Fragment><h1>Hello world !!!</h1><p>i am learning reactjs</p><h2>i will definitely get 12 lpa</h2></React.Fragment>, document.getElementById("root"));
+
+// const username= "anshul"    //react js expressions
+// ReactDOM.render(<><h1>my name is {username}</h1><p>{3+2}</p></>,document.getElementById("root"));
+
+//we cant use statements in braces (if else etc)
+
+
+// template literals
+
+const username= "anshul";
+const lastname= "tripathi"    
+ReactDOM.render(<><h1>{`my name is ${username} ${lastname}`}</h1></>,document.getElementById("root"));
