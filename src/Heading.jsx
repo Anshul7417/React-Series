f471ff086c4f3ef0d11d5947a6ca80f966e8d442
Anshul Tripathi <tripathianshul06@gmail.com>
2022-05-16 21:59:47 +0530

    Started Again

---
+++ src/Heading.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// filename must be capital and function name (component must be same)
+function Heading(){
+    return <h1> My name is Anshul </h1>
+}
+
+export default Heading;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import './index.css'     // adding css
+// import Heading from './Heading.jsx';    // Component
+// import Para from './Para.jsx';          // Component
 
-let curDate = new Date;
-curDate =curDate.getHours();
-let greeting ="";
+import App from './App';
 
-if (curDate >= 1 && curDate <12) {
-    greeting='good morning';
-} else if (curDate >=12 && curDate <19) {
-    greeting='good Afternoon';
-} else{
-    greeting='Good Night';
-}
+// let curDate = new Date(2020,5,5,15);
+// curDate =curDate.getHours();
+// let greeting ="";
+// const cssStyle ={
+// };
+
+// if (curDate >= 1 && curDate <12) {
+//     greeting='good morning';
+//     cssStyle.color ='green';
+// } else if (curDate >=12 && curDate <19) {
+//     greeting='good Afternoon';
+//     cssStyle.color ='red';
+// } else{
+//     greeting='Good Night';
+//     cssStyle.color ='black';
+// }
+
+// ReactDOM.render(<h1>Hello sir ,<span style ={cssStyle}>{greeting} </span></h1>,document.getElementById('root'));
+
+// const img1="https://picsum.photos/200/300";
+
+// ReactDOM.render(<><h1 contentEditable="true" className='Header'> Hello Sir , Good Morning</h1><img src={img1} alt='not available'/></>,document.getElementById('root')); // attributes in jsx has a camel case while in normal html it is normal......yaha pr single elements(no closing tags) ko </> aese likhna odta hai..see img tag.......className is class in react
+
+// const heading ={               // method of adding inline css in react..........property uses camelcase here
+//     color: "#fa9191",
+//     textTransform : 'capitalize'    // see this property in css also
+// }
+
+
+// const heading2 = {   // for eference to how to convert to object perfectly
+//     color: "#fa9191",
+//     textTransform: "capitalize",
+//     textAlign: "center",
+//     fontWeight: 'bold',
+//     textShadow: '0px 2px 4px #ffe9c5',
+//     margin: '70px ',
+//     fontFamily: '"Josefin Sans", sans-serif'
+//     };
+
+// ReactDOM.render(<><h1 style={{ color: "#fa9191",
+// textTransform : 'capitalize' }}>hello anshul</h1></>,document.getElementById('root'));          // ek hi react dom kaam krta hai  style ={haeding}  likhne se bhi hoga kaam
 
 
 
-ReactDOM.render(<h1> Hello Sir , Good Morning</h1>,document.getElementById('root'))
+// Components
+
+// ReactDOM.render(<><h1>My name is Anshul Tripathi</h1><Heading />
+//     <p>This is my world</p><Para/><ol><li>Anshul</li><li>Tripathi</li></ol></>, document.getElementById('root'));
+
+
+
+ReactDOM.render(
+    <><App/></>,document.getElementById('root')
+);
+
+
+
+
