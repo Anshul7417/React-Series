5e38dfb48d6cbe6d78000ffa94f7d2f7db70eb0f
Anshul Tripathi <tripathianshul06@gmail.com>
2022-07-11 11:19:42 +0530

    Started again

--- extra2/src/Prop.jsx
+++ extra2/src/Prop.jsx
@@ -7,7 +7,7 @@ function Prop(props) {
                     {props.title}
                 </span>
                 <h3 className='card_title'>{props.sname}</h3>
-                <a href={props.link}target='_blank'><button>Watch now
+                <a href={props.link} target='_blank'><button>Watch now
                 </button></a>
             </div>
         </div>
